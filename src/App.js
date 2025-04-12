a435228fd33810082a76235d9e841dc91e88a0ea
Jason <jdaly398@gmail.com>
2025-04-12 16:40:52 +0100

    testing deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home"
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact></Route>
+          <Route path="/" exact Component={Home}></Route>
         </Routes>
       </Router>
     </div>
