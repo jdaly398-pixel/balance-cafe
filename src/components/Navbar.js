1be72242f45baf593594a61d1e3d4a0030ac0c1b
Jason <jdaly398@gmail.com>
2025-04-15 20:58:01 +0100

    updatednavbar and fixed redirect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,25 +7,32 @@ import Logo from "../assets/balanaceLogo.png";
 function Navbar() {
   const [openLinks, setOpenLinks] = useState(false);
 
+  // Function to toggle navbar
   const toggleNavbar = () => {
     setOpenLinks(!openLinks);
-  }
+  };
+
+  // Function to handle link clicks
+  const handleLinkClick = () => {
+    setOpenLinks(false); // Reset navbar to show logo
+  };
+
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
         <img src={Logo} alt="Logo" />
         <div className="hiddenLinks">
-          <Link to={"/"}>Home</Link>
-          <Link to={"/menu"}>Menu</Link>
-          <Link to={"/about"}>About</Link>
-          <Link to={"/contact"}>Contact</Link>
+          <Link to={"/"} onClick={handleLinkClick}>Home</Link>
+          <Link to={"/menu"} onClick={handleLinkClick}>Menu</Link>
+          <Link to={"/about"} onClick={handleLinkClick}>About</Link>
+          <Link to={"/contact"} onClick={handleLinkClick}>Contact</Link>
         </div>
       </div>
       <div className="rightSide">
-        <Link to={"/"}>Home</Link>
-        <Link to={"/menu"}>Menu</Link>
-        <Link to={"/about"}>About</Link>
-        <Link to={"/contact"}>Contact</Link>
+        <Link to={"/"} onClick={handleLinkClick}>Home</Link>
+        <Link to={"/menu"} onClick={handleLinkClick}>Menu</Link>
+        <Link to={"/about"} onClick={handleLinkClick}>About</Link>
+        <Link to={"/contact"} onClick={handleLinkClick}>Contact</Link>
         <button onClick={toggleNavbar}>
           <ReorderIcon />
         </button>
